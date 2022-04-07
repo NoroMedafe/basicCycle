101ecf88f47134fd8d0a7a86c2ce01e6ce294886
NoroMedafe <danya1515@inbox.ru>
2022-04-07 16:47:25 +0300

    Добавьте файлы проекта.

---
+++ basicCycle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRepetitions;
+            string userName;
+
+            Console.Write("Выведем ваще имя столько раз сколько захотите!" + "\n" + "Введите имя: ");
+            userName = Console.ReadLine();
+            Console.Write("Сколько раз вывести?: "); numberOfRepetitions= Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberOfRepetitions; i++)
+            {
+                Console.Write(userName + " ");
+            }
+            Console.WriteLine("Всё!");
+            Console.ReadKey();
+        }
+    }
+}
